9c7c423247f0dc8da20c584069ef9fb574659564
Stephen Seo <seo.disparate@gmail.com>
2020-08-09 16:14:33 +0900

    Fix build with MS Visual Studio

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,22 @@
 
 #include "state.hpp"
 
+#ifdef _MSC_VER
+#include "windows.h"
+
+int WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) {
+    // init
+    Tri::State state(0, nullptr);
+
+    // main loop
+    while(state.get_flags().test(1)) {
+        state.handle_events();
+        state.update();
+        state.draw();
+    }
+    return 0;
+}
+#else
 int main(int argc, char **argv) {
     // init
     Tri::State state(argc, argv);
@@ -20,3 +36,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+#endif
